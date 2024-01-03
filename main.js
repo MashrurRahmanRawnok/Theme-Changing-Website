c6b95738a72148ad86db35cece01d887395e788a
Mashrur Rahman <MashrurRahmanRawnok@proton.me>
2024-01-03 23:56:14 +0600

    Uploded The Files

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById("Red").onclick = function() {
+    document.body.style.backgroundColor = "red"
+    document.getElementById("f1").style.color = "white"
+};
+document.getElementById("Blue").onclick = function() {
+    document.body.style.backgroundColor = "blue"
+    document.getElementById("f1").style.color = "white"
+};
+document.getElementById("Green").onclick = function() {
+    document.body.style.backgroundColor = "green"
+    document.getElementById("f1").style.color = "white"
+};
+document.getElementById("Black").onclick = function() {
+    document.body.style.backgroundColor = "black"
+    document.getElementById("f1").style.color = "white"
+};
+document.getElementById("White").onclick = function() {
+    document.body.style.backgroundColor = "white"
+    document.getElementById("f1").style.color = "black"
+};
+document.getElementById("Yellow").onclick = function() {
+    document.body.style.backgroundColor = "yellow"
+    document.getElementById("f1").style.color = "black"
+};
+document.getElementById("Purple").onclick = function() {
+    document.body.style.backgroundColor = "purple"
+    document.getElementById("f1").style.color = "white"
+};
+document.getElementById("Orange").onclick = function() {
+    document.body.style.backgroundColor = "orangered"
+    document.getElementById("f1").style.color = "white"
+};
+document.getElementById("Pink").onclick = function() {
+    document.body.style.backgroundColor = "pink"
+    document.getElementById("f1").style.color = "black"
+};
+document.getElementById("Brown").onclick = function() {
+    document.body.style.backgroundColor = "brown"
+    document.getElementById("f1").style.color = "white"
+};
